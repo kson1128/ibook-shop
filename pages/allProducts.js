aa2383a9ac866f641e24fcb1c27b054cbca135cb
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-09-19 12:35:37 -0400

    fix:deployment errors

--- pages/allProducts.js
+++ pages/allProducts.js
@@ -2,10 +2,8 @@ import axios from 'axios';
 import SingleProduct from '../components/singleProduct';
 import styles from '../styles/allProducts.module.css';
 import BookList from '../components/allBooks';
-import supabase from './../supabase/**utils**/supabase';
 
 export const getServerSideProps = async context => {
-  console.log('supa', supabase);
   const res = await axios.get(`http://localhost:8000/api/allProducts`);
   return {
     props: {
