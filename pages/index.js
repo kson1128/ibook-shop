374193e9a4b1a9507f289c77091cf4a8767730c5
kson1128 <69124193+kson1128@users.noreply.github.com>
2022-09-20 01:13:00 -0400

    fix for deploy part 4

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import CarouselHome from '../components/carousel';
 import Icons from '../components/iconsGroup';
+import { supabase } from '../public/utils/supabase';
 
 export default function Home({ bookList, admin }) {
   const [close, setClose] = useState(true);
@@ -14,10 +15,11 @@ export default function Home({ bookList, admin }) {
 }
 
 export const getServerSideProps = async context => {
-  const res = await axios.get('http://localhost:8000/api/allProducts');
+  // const res = await axios.get('http://localhost:8000/api/allProducts');
+  let { data: Product, error } = await supabase.from('Product').select();
   return {
     props: {
-      bookList: res.data,
+      bookList: Product,
       // admin,
     },
   };
